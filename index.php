677a78244272e1df2b68c783faec04e5764b52c2
Tlachev Maksim <tkachevm@ya.ru>
2022-01-30 12:58:32 +0300

    1

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<section>
+    <div>
+        <lable for="suggest">Поиск</lable>
+        <input type="text" id="suggest">
+    </div>
+    <div>
+        <div id="summ">Сумма</div>
+        <div id="km">Расстояние</div>
+    </div>
+</section>
+<section >
+    <div class="aside">
+        Меню
+    </div>
+    <div class="container map">
+        <div id="map" style="height: 300px;margin-bottom:30px;"></div>
+    </div>
+</section>
+</body>
+</html>
